7d8be527a399cd32d94703e90ca06a66899e28c4
roger lee <lwzz0038@163.com>
2021-12-18 17:28:37 +0800

    edit step3.v

--- step3.v
+++ step3.v
@@ -1,16 +1,16 @@
 module step3 (clk,action)
 parameter num_s=2
-parameter num_b=100;
+parameter num_b=16;
 input clk;
 output action;
 reg V;
 reg[15:0] s0_b[num_b-1,0];
 reg[15:0] s1_b[num_b-1,0];
 reg[15:0] gamma_a1_b_s0[num_b-1,0];
-reg[15:0] gamma_a2_bs_s0[num_b-1,0];
+reg[15:0] gamma_a2_b_s0[num_b-1,0];
 reg[15:0] gamma_a3_b_s0[num_b-1,0];
 reg[15:0] gamma_a1_b_s1[num_b-1,0];
-reg[15:0] gamma_a2_bs_s1[num_b-1,0];
+reg[15:0] gamma_a2_b_s1[num_b-1,0];
 reg[15:0] gamma_a3_b_s1[num_b-1,0];
 
 always@ (posedge clk)
