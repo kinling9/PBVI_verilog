d93b1cdf9a9458c4a02ddbe6a4880c9cd02a5f47
roger lee <lwzz0038@163.com>
2021-12-19 14:12:26 +0800

    add testbench

--- step3.v
+++ step3.v
@@ -1,19 +1,74 @@
-module step3 (clk,action)
-parameter num_s=2
+module step3 (clk,en,action);
+parameter num_s=2;
 parameter num_b=16;
 input clk;
-output action;
-reg V;
-reg[15:0] s0_b[num_b-1,0];
-reg[15:0] s1_b[num_b-1,0];
-reg[15:0] gamma_a1_b_s0[num_b-1,0];
-reg[15:0] gamma_a2_b_s0[num_b-1,0];
-reg[15:0] gamma_a3_b_s0[num_b-1,0];
-reg[15:0] gamma_a1_b_s1[num_b-1,0];
-reg[15:0] gamma_a2_b_s1[num_b-1,0];
-reg[15:0] gamma_a3_b_s1[num_b-1,0];
+input en;
+output reg[1:0] action;
+integer i;
+reg[32:0] temp;
+reg[32:0] V_b[num_b-1:0];
+reg[15:0] s0_b[num_b-1:0];
+reg[15:0] s1_b[num_b-1:0];
+reg[15:0] best_gamma_b_s0;
+reg[15:0] best_gamma_b_s1;
+reg[15:0] gamma_a1_b_s0[num_b-1:0];
+reg[15:0] gamma_a2_b_s0[num_b-1:0];
+reg[15:0] gamma_a3_b_s0[num_b-1:0];
+reg[15:0] gamma_a1_b_s1[num_b-1:0];
+reg[15:0] gamma_a2_b_s1[num_b-1:0];
+reg[15:0] gamma_a3_b_s1[num_b-1:0];
+reg[15:0] alpha_vector_b_s0[num_b-1:0];
+reg[15:0] alpha_vector_b_s1[num_b-1:0];
 
-always@ (posedge clk)
+initial 
 begin
-     
+    best_gamma_b_s0=0;
+    best_gamma_b_s1=0;
+    temp=0;
+    action=0;
+    for(i=0;i<num_b;i=i+1)
+    begin
+        V_b[i]=0;
+        gamma_a1_b_s0[i]=1;
+        gamma_a2_b_s0[i]=2;
+        gamma_a3_b_s0[i]=3;
+        gamma_a1_b_s1[i]=1;
+        gamma_a2_b_s1[i]=2;
+        gamma_a3_b_s1[i]=3;
+        alpha_vector_b_s0[i]=0;
+        alpha_vector_b_s1[i]=0;
+    end
 end
+
+always @(posedge clk)
+if (en)
+begin
+     for(i=0;i<num_b;i=i+1)
+     begin
+         temp=s0_b[i]*gamma_a1_b_s0[i]+s1_b[i]*gamma_a1_b_s1[i];
+         if (temp>V_b[i])
+           V_b[i]=temp;
+           alpha_vector_b_s0[i]=gamma_a1_b_s0[i];
+           alpha_vector_b_s1[i]=gamma_a1_b_s1[i];
+           action=2'b01;
+     end
+     for(i=0;i<num_b;i=i+1)
+     begin
+         temp=s0_b[i]*gamma_a2_b_s0[i]+s1_b[i]*gamma_a2_b_s1[i];
+         if (temp>V_b[i])
+           V_b[i]=temp;
+           alpha_vector_b_s0[i]=gamma_a2_b_s0[i];
+           alpha_vector_b_s1[i]=gamma_a2_b_s1[i];
+           action=2'b10;
+     end  
+     for(i=0;i<num_b;i=i+1)
+     begin
+         temp=s0_b[i]*gamma_a3_b_s0[i]+s1_b[i]*gamma_a3_b_s1[i];
+         if (temp>V_b[i])
+           V_b[i]=temp;
+           alpha_vector_b_s0[i]=gamma_a3_b_s0[i];
+           alpha_vector_b_s1[i]=gamma_a3_b_s1[i];
+           action=2'b11;
+     end  
+end
+endmodule
---
+++ step3_testbench.v
@@ -0,0 +1,22 @@
+`timescale  1ns/1ns
+module step3_tb ();
+reg clk;
+reg en;
+wire action;
+
+initial 
+begin
+    clk=0;
+    en=0;
+    #500 en=1;
+end
+
+always#100 clk=~clk;
+
+step3 dut
+(
+    .clk(clk),
+    .en(en),
+    .action(action)
+);
+endmodule
