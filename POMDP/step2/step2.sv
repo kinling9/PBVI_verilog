8d58d0c0f1cb457076d90f3377813d98e719d363
0Minagi0 <xingyutong1999@gmail.com>
2021-12-23 23:41:27 +0800

    step2.sv: swap [i] and [j] for debug

--- POMDP/step2/step2.sv
+++ POMDP/step2/step2.sv
@@ -14,12 +14,11 @@ parameter STATE_LEVEL1 = 3'b001;
 parameter STATE_LEVEL2 = 3'b010;
 parameter STATE_LEVEL3 = 3'b011;
 parameter STATE_LEVEL4 = 3'b100;
-parameter STATE_STOP = 3'b101;
+parameter STATE_OUTPUT = 3'b101;
+parameter STATE_STOP = 3'b110;
 
 logic [2:0] state;
-logic [15:0] gamma_intermediate_dot_point_belief [0:2][0:1][0:15][0:15];
-logic [31:0] gamma_intermediate_dot_point_belief_32 [0:2][0:1][0:15][0:15];
-//logic [15:0] sel_dot [0:2][0:1][0:7][0:15];
+logic [31:0] gamma_intermediate_dot_point_belief [0:2][0:1][0:15][0:15];
 //logic [3:0] alpha_idx [0:2][0:1][0:15][0:15];
 logic [3:0] sel_alpha_idx [0:2][0:1][0:15][0:15];
 
@@ -43,11 +42,10 @@ always_comb begin
         for(int j = 0; j < 16; ++j) begin
             for(int k = 0; k < 2; ++k) begin
                 for(int l = 0; l<3; ++l) begin
-                    gamma_intermediate_dot_point_belief_32[l][k][j][i] =
+                    gamma_intermediate_dot_point_belief[l][k][i][j] =
                     gamma_intermediate_action_observation_alpha[l][k][j][0]*point_belief[i][0] +
                     gamma_intermediate_action_observation_alpha[l][k][j][1]*point_belief[i][1];
-                    gamma_intermediate_dot_point_belief[l][k][j][i] = gamma_intermediate_dot_point_belief_32[l][k][j][i][31-:16];
-                    sel_alpha_idx[l][k][j][i] <= j;
+                    sel_alpha_idx[l][k][i][j] <= j;
                 end
             end
         end
@@ -58,16 +56,16 @@ always_comb begin
         for(int j = 0; j < 8; ++j) begin
           for(int k=0; k < 2; ++k) begin
             for(int l=0; l < 3; ++l) begin
-                gamma_intermediate_dot_point_belief[l][k][j][i] <= 
-                (gamma_intermediate_dot_point_belief[l][k][2*j][i] >= 
-                  gamma_intermediate_dot_point_belief[l][k][2*j+1][i]) ?
-                  gamma_intermediate_dot_point_belief[l][k][2*j][i] :
-                  gamma_intermediate_dot_point_belief[l][k][2*j+1][i];
-                sel_alpha_idx[l][k][j][i] <=
-                (gamma_intermediate_dot_point_belief[l][k][2*j][i] >= 
-                  gamma_intermediate_dot_point_belief[l][k][2*j+1][i]) ?
-                  sel_alpha_idx[l][k][2*j][i] :
-                  sel_alpha_idx[l][k][2*j+1][i];
+                gamma_intermediate_dot_point_belief[l][k][i][j] <= 
+                (gamma_intermediate_dot_point_belief[l][k][i][2*j] >= 
+                  gamma_intermediate_dot_point_belief[l][k][i][2*j+1]) ?
+                  gamma_intermediate_dot_point_belief[l][k][i][2*j] :
+                  gamma_intermediate_dot_point_belief[l][k][i][2*j+1];
+                sel_alpha_idx[l][k][i][j] <=
+                (gamma_intermediate_dot_point_belief[l][k][i][2*j] >= 
+                  gamma_intermediate_dot_point_belief[l][k][i][2*j+1]) ?
+                  sel_alpha_idx[l][k][i][2*j] :
+                  sel_alpha_idx[l][k][i][2*j+1];
             end
           end
         end
@@ -76,60 +74,72 @@ always_comb begin
     STATE_LEVEL2: begin
       for(int i = 0; i < 16; ++i) begin
         for(int j = 0; j < 4; ++j) begin
-            for(int k=0; k < 2; ++k) begin
-                for(int l=0; l < 3; ++l) begin
-                    gamma_intermediate_dot_point_belief[l][k][j][i] <= 
-                    (gamma_intermediate_dot_point_belief[l][k][2*j][i] >= 
-                     gamma_intermediate_dot_point_belief[l][k][2*j+1][i]) ?
-                     gamma_intermediate_dot_point_belief[l][k][2*j][i] :
-                     gamma_intermediate_dot_point_belief[l][k][2*j+1][i];
-                    sel_alpha_idx[l][k][j][i] <=
-                    (gamma_intermediate_dot_point_belief[l][k][2*j][i] >= 
-                     gamma_intermediate_dot_point_belief[l][k][2*j+1][i]) ?
-                     sel_alpha_idx[l][k][2*j][i] :
-                     sel_alpha_idx[l][k][2*j+1][i];
-                end
+          for(int k=0; k < 2; ++k) begin
+            for(int l=0; l < 3; ++l) begin
+            gamma_intermediate_dot_point_belief[l][k][i][j] <= 
+            (gamma_intermediate_dot_point_belief[l][k][i][2*j] >= 
+              gamma_intermediate_dot_point_belief[l][k][i][2*j+1]) ?
+              gamma_intermediate_dot_point_belief[l][k][i][2*j] :
+              gamma_intermediate_dot_point_belief[l][k][i][2*j+1];
+            sel_alpha_idx[l][k][i][j] <=
+            (gamma_intermediate_dot_point_belief[l][k][i][2*j] >= 
+              gamma_intermediate_dot_point_belief[l][k][i][2*j+1]) ?
+              sel_alpha_idx[l][k][i][2*j] :
+              sel_alpha_idx[l][k][i][2*j+1];
             end
+          end
         end
       end
     end
     STATE_LEVEL3: begin
       for(int i = 0; i < 16; ++i) begin
         for(int j = 0; j < 2; ++j) begin
-            for(int k=0; k < 2; ++k) begin
-                for(int l=0; l < 3; ++l) begin
-                    gamma_intermediate_dot_point_belief[l][k][j][i] <= 
-                    (gamma_intermediate_dot_point_belief[l][k][2*j][i] >= 
-                     gamma_intermediate_dot_point_belief[l][k][2*j+1][i]) ?
-                     gamma_intermediate_dot_point_belief[l][k][2*j][i] :
-                     gamma_intermediate_dot_point_belief[l][k][2*j+1][i];
-                    sel_alpha_idx[l][k][j][i] <=
-                    (gamma_intermediate_dot_point_belief[l][k][2*j][i] >= 
-                     gamma_intermediate_dot_point_belief[l][k][2*j+1][i]) ?
-                     sel_alpha_idx[l][k][2*j][i] :
-                     sel_alpha_idx[l][k][2*j+1][i];
-                end
+          for(int k=0; k < 2; ++k) begin
+            for(int l=0; l < 3; ++l) begin
+            gamma_intermediate_dot_point_belief[l][k][i][j] <= 
+            (gamma_intermediate_dot_point_belief[l][k][i][2*j] >= 
+              gamma_intermediate_dot_point_belief[l][k][i][2*j+1]) ?
+              gamma_intermediate_dot_point_belief[l][k][i][2*j] :
+              gamma_intermediate_dot_point_belief[l][k][i][2*j+1];
+            sel_alpha_idx[l][k][i][j] <=
+            (gamma_intermediate_dot_point_belief[l][k][i][2*j] >= 
+              gamma_intermediate_dot_point_belief[l][k][i][2*j+1]) ?
+              sel_alpha_idx[l][k][i][2*j] :
+              sel_alpha_idx[l][k][i][2*j+1];
             end
+          end
         end
       end
     end
     STATE_LEVEL4: begin
       for(int i = 0; i < 16; ++i) begin
         for(int j = 0; j < 1; ++j) begin
-            for(int k=0; k < 2; ++k) begin
-                for(int l=0; l < 3; ++l) begin
-                    gamma_intermediate_dot_point_belief[l][k][j][i] <= 
-                    (gamma_intermediate_dot_point_belief[l][k][2*j][i] >= 
-                     gamma_intermediate_dot_point_belief[l][k][2*j+1][i]) ?
-                     gamma_intermediate_dot_point_belief[l][k][2*j][i] :
-                     gamma_intermediate_dot_point_belief[l][k][2*j+1][i];
-                    sel_alpha_idx[l][k][j][i] <=
-                    (gamma_intermediate_dot_point_belief[l][k][2*j][i] >= 
-                     gamma_intermediate_dot_point_belief[l][k][2*j+1][i]) ?
-                     sel_alpha_idx[l][k][2*j][i] :
-                     sel_alpha_idx[l][k][2*j+1][i];
-                end
+          for(int k=0; k < 2; ++k) begin
+            for(int l=0; l < 3; ++l) begin
+            gamma_intermediate_dot_point_belief[l][k][i][j] <= 
+            (gamma_intermediate_dot_point_belief[l][k][i][2*j] >= 
+              gamma_intermediate_dot_point_belief[l][k][i][2*j+1]) ?
+              gamma_intermediate_dot_point_belief[l][k][i][2*j] :
+              gamma_intermediate_dot_point_belief[l][k][i][2*j+1];
+            sel_alpha_idx[l][k][i][j] <=
+            (gamma_intermediate_dot_point_belief[l][k][i][2*j] >= 
+              gamma_intermediate_dot_point_belief[l][k][i][2*j+1]) ?
+              sel_alpha_idx[l][k][i][2*j] :
+              sel_alpha_idx[l][k][i][2*j+1];
             end
+          end
+        end
+      end
+    end 
+    STATE_OUTPUT: begin
+      for(int i = 0; i < 16; ++i) begin
+        for(int l = 0; l < 3; ++l) begin
+          for(int s = 0; s < 2; ++s) begin
+            gamma_action_belief[l][i][s] = 
+            gamma_reward_action[l][s] +
+            gamma_intermediate_action_observation_alpha[l][0][sel_alpha_idx[l][0][i][0]][s] +
+            gamma_intermediate_action_observation_alpha[l][1][sel_alpha_idx[l][1][i][0]][s];
+          end
         end
       end
     end 
@@ -148,23 +158,23 @@ always_comb begin
   endcase
 end
 
-always_comb begin
-  if (state == STATE_STOP) begin
-    for(int i = 0; i < 16; ++i) begin
-      for(int l = 0; l < 3; ++l) begin
-        for(int s = 0; s < 2; ++s) begin
-          gamma_action_belief[l][i][s] = 
-          gamma_reward_action[l][s] +
-          gamma_intermediate_action_observation_alpha[l][0][sel_alpha_idx[l][0][0][i]][s] +
-          gamma_intermediate_action_observation_alpha[l][1][sel_alpha_idx[l][1][0][i]][s];
-        end
-      end
-    end
-  end
-end
+// always_comb begin
+//   if (state == STATE_STOP) begin
+//     for(int i = 0; i < 16; ++i) begin
+//       for(int l = 0; l < 3; ++l) begin
+//         for(int s = 0; s < 2; ++s) begin
+//           gamma_action_belief[l][i][s] = 
+//           gamma_reward_action[l][s] +
+//           gamma_intermediate_action_observation_alpha[l][0][sel_alpha_idx[l][0][0][i]][s] +
+//           gamma_intermediate_action_observation_alpha[l][1][sel_alpha_idx[l][1][0][i]][s];
+//         end
+//       end
+//     end
+//   end
+// end
 
 always_comb begin
-  if (state == STATE_LEVEL4) begin
+  if (state == STATE_OUTPUT) begin
     en_step3 = 1'b1;
   end else if (state == STATE_STOP) begin
     en_step3 = 1'b0;
